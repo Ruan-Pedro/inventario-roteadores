b5f74a00a4333469bfce6330b03c99e5dda60b71
Ruan Pedro <ruanpmp@gmail.com>
2021-07-25 17:40:20 -0300

    last commits

---
+++ ng-routers/src/app/directives/dbfor.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DbforDirective } from './dbfor.directive';
+
+describe('DbforDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DbforDirective();
+    expect(directive).toBeTruthy();
+  });
+});
